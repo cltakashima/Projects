c3482fe979adb5e21b72c26bf011a377f0416881
cltakashima <cltakashima@users.noreply.github.com>
2024-02-01 16:18:24 +0100

    Add files via upload

---
+++ W3_Database.sql
@@ -0,0 +1,49 @@
+/*The company is a mock company using real-world data using W3 Schools Database
+
+The company wanted to compensate the 5 top sales team performers
+
+I used SQL to:
+
+Identify how the information was organized
+sort and filter the data
+determine the credibility of the data
+
+The audience is the CFO of the company
+
+I will create queries and extract tables to show the top five performers based on two different criteria*/
+
+-- Key Takeaways
+
+-- TOP 5 SALES *(Possible same employees)
+-- This query lists the top five sales ever made.  Some of these sales may have been made by the same employee
+
+SELECT LastName, FirstName, Orders.OrderID, Products.ProductID, Quantity, Price, SUM(Quantity * Price) AS SalesAmount
+FROM Employees
+	INNER JOIN Orders
+		ON Employees.EmployeeID = Orders.EmployeeID
+	INNER JOIN OrderDetails
+		ON OrderDetails.OrderID  = Orders.OrderID
+	INNER JOIN Products
+		ON Product.ProductID = OrderDetails.ProductID
+GROUP BY Orders.OrderID
+ORDER BY  SalesAmount DESC
+LIMIT 5
+
+-- TOP 5 SALES EMPLOYEES
+-- This query lists the top five sales employees
+
+SELECT LastName, FirstName, Orders.OrderID, Products.ProductID, Quantity, Price, SUM(Quantity * Price) AS SalesAmount
+FROM Employees
+	INNER JOIN Orders
+		ON Employees.EmployeeID = Orders.EmployeeID
+	INNER JOIN OrderDetails
+		ON OrderDetails.OrderID  = Orders.OrderID
+	INNER JOIN Products
+		ON Product.ProductID = OrderDetails.ProductID
+GROUP BY Orders.OrderID
+HAVING Orders.OrderID IN (10372, 10424, 10417, 10324, 10351)
+ORDER BY  SalesAmount DESC
+
+
+
+
